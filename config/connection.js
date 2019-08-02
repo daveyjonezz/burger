f8cbf2ae415b54b59f7dafb9b8797e62007dfabf
Davis Birsan <davis.birsan@gmail.com>
2019-08-01 20:28:10 -0500

    Updated JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 require('dotenv').config();
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.SECRET_USER,
     password: process.env.SECRET_KEY,
     database: "burgers_db"
-});
+})
+};
 // Make connection.
 connection.connect(function(err) {
     if (err) {
